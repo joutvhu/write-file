8a69b3893f3572f43385ea601940d3cb324a0410
Giao Ho <joutvhu@gmail.com>
2021-06-17 02:51:45 +0700

    Write file

--- src/io-helper.ts
+++ src/io-helper.ts
@@ -12,19 +12,19 @@ export interface WriteFileInputs {
  * Helper to get all the inputs for the action
  */
 export function getInputs(): WriteFileInputs {
-    const result: WriteFileInputs | any = {
-        path: ".",
-        contents: "",
-        mode: "overwrite"
-    };
+    const result: WriteFileInputs | any = {};
 
     result.path = core.getInput(Inputs.Path, {required: true});
 
     result.contents = core.getInput(Inputs.Contents, {required: true});
 
     result.mode = core.getInput(Inputs.Mode, {required: true});
+    if (!['append', 'overwrite', 'preserve'].includes(result.mode))
+        result.mode = 'overwrite';
 
     result.encoding = core.getInput(Inputs.Encoding, {required: false});
+    if (!['ascii', 'utf8', 'utf-8', 'utf16le', 'ucs2', 'ucs-2', 'base64', 'base64url', 'latin1', 'binary', 'hex'].includes(result.encoding))
+        delete result.encoding;
 
     return result;
 }
--- src/write-file.ts
+++ src/write-file.ts
@@ -1,9 +1,23 @@
 import * as core from '@actions/core';
+import {mkdirP} from '@actions/io';
+import {appendFileSync, existsSync, writeFileSync} from 'fs';
+import {dirname} from 'path';
 import {getInputs, WriteFileInputs} from './io-helper';
 
 (async function run() {
     try {
         const inputs: WriteFileInputs = getInputs();
+
+        if (inputs.mode !== 'preserve' || !existsSync(inputs.path)) {
+            const targetDir = dirname(inputs.path);
+            await mkdirP(targetDir);
+
+            if (inputs.mode === 'overwrite') {
+                writeFileSync(inputs.path, inputs.contents);
+            } else {
+                appendFileSync(inputs.path, inputs.contents);
+            }
+        }
     } catch (err) {
         core.setFailed(err.message);
     }
